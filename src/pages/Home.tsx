759f5e625c272bddbe6e7245a76dbcd8522d5e42
aaqibraza92 <78475535+aaqibraza92@users.noreply.github.com>
2025-02-17 16:24:15 +0530

    u

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,12 +12,79 @@ function Home() {
       return `${a} is type ${typeof (a)}`
     }
   }
-  console.log(welMsg(12));
+
+
+  type emp1={
+  name: string,
+  department: string,
+  phone: number
+  }
+
+  type emp2={
+    state: string,
+    pin: number | string,
+    adult: boolean,
+    addr: (string | number)[]
+    }
+    // type bothEmpl= emp1 & emp2;
+
+    let student:emp1 & emp2={
+      name: "aaqib",
+      department: "IT",
+      phone: 8269321549,
+      state: "mp",
+      pin: "45216",
+      adult: true,
+      addr: ["a","b"]
+
+    }
+    console.log("student",student)
+
+    // const generaicFunc:<T>=(a:T)=>:T{
+    //   return a;
+    // }
+
+  // const genericVal=<T>(a:T):T=>{
+  //   return a;
+  // }
+
+
+
+  const myFunction = <T, U>(param1: T, param2: U): {first: T, second: U} => {
+    return {first: param1,second: param2};
+};
+
+// Example usage
+const result = myFunction<string, number>('Hello', 42);
+console.log(result); // Output: ['Hello', 42]
+
+
+
+
+// function genericFunc<T>(a:T):T{
+//   return a;
+// }
+
+// const genericFunc = <T extends unknown>(x: T) => x;
+
+const genericFunc=<T extends number,U extends number>(a:T,b:U):any=>{
+  return a+b
+  }
+  const result1 = genericFunc<number,number>(4,5);
+  
+  const currentDateTime = new Date()
+
+console.log(currentDateTime.getFullYear()+":"+currentDateTime.getMinutes());
+
 
   return (
+    <>
+    
     <h2>
       Hello
     </h2>
+    
+    </>
 
   );
 }
