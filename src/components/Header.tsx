73070138b8643d204ce29a746d7ddd31971077ee
aaqibraza92 <78475535+aaqibraza92@users.noreply.github.com>
2025-03-28 17:55:25 +0530

    u

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
 
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import Box from '@mui/material/Box';
 
 import headerStyle from "../assets/css/header.module.css";
 import HeaderDesktop from "./HeaderDesktop";
 import HeaderMobile from "./HeaderMobile";
 
+
+const MyComponent = memo(({ count }: { count: number }) => {
+  console.log("Component re-rendered");
+  return <h2>Count: {count}</h2>;
+});
+
 const Header: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+  const [headerCount, setheaderCount] = useState<number>(0);
+
   const [width, setWidth] = useState<number>(window.innerWidth)
 
   const resizeScreen = () => {
@@ -24,6 +33,10 @@ const Header: React.FC = () => {
 
   return (
     <>
+    {headerCount}
+    <button onClick={()=>setheaderCount(headerCount + 1)}>header update</button>
+       <MyComponent count={count} />
+       <button onClick={() => setCount(count + 1)}>Increment</button>
       <Box className={headerStyle.topHeader}>
         Free Shipping All Over The World
       </Box>
